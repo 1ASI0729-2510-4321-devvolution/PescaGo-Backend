272ad6a6f7457af50c2f87fc84b02ab75fffe561
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-06-04 22:57:29 -0500

    Initial commit

---
+++ src/main/java/pe/upc/pescagobackend/PescagoBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.pescagobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PescagoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PescagoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/upc/pescagobackend/PescagoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.pescagobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PescagoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
